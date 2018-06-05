9bf243c3e8180c638b6e28868cd336a171d333a8
Thimon Johannsen <39953579+Thyral@users.noreply.github.com>
2018-06-05 14:56:05 +0200

    Add files via upload

---
+++ Aufgabe_1/src/CowSay.java
@@ -0,0 +1,20 @@
+
+public class CowSay {
+	
+	public static void main (String args [])
+	{
+		String rocks;
+		
+		rocks = "< Java rocks! >";
+		
+		System.out.println("__________________");
+		System.out.println(rocks );
+		System.out.println("------------------");
+		System.out.println("\\  " + " ^__^");
+		System.out.println("\\  " + " (oo)\\________");
+		System.out.println("    " + "(__)\\       )\\/\\");
+		System.out.println("    " + "    ||----W |");
+		System.out.println("    " + "    ||     ||");
+	}
+
+}
